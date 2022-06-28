74135d443df6812b7c9842aded32dfefb5aecae8
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-06-28 12:39:11 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Armstrong
+
+{
+
+  public static void main(String[]args)
+
+       {
+
+     Scanner sc =new Scanner(System.in);
+
+     System.out.print("Enter Three Digit Number: ");
+
+    int N=sc.nextInt();
+
+     int Sum=0, i=N;
+
+while(i!=0)
+{
+
+ int rem=i%10;
+ 
+ Sum=Sum+(rem*rem*rem);
+ 
+ i=i/10;
+}
+if(N==Sum)
+System.out.println("Enter Number is Armstrong Number");
+else
+System.out.println("Enter Number is not Armstrong Number");
+    }
+
+}
+
